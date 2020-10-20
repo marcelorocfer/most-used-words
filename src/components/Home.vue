fe46231586e64a9a23d1f1d70ad982f019271cf8
Marcelo Rocha Ferreira <marceloengecomp@gmail.com>
2020-10-20 18:43:30 -0300

    Finalizando detalhes da criação da interface gráfica.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
     <v-container fluid>
+        <v-form>
+            <v-file-input
+                label="Selecione as Legendas"
+                prepend-icon="mdi-message-text"
+                append-outer-icon="mdi-send"
+                outlined
+                multiple
+                chips
+                v-model="files"
+                @click:append-outer="processSubtitles"
+            />
+        </v-form>
         <div class="pills">
             <Pill v-for="word in groupedWords" :key="word.name"
                 :name="word.name" :amount="word.amount"
@@ -15,6 +27,7 @@
         components: { Pill },
         data: function() {
             return {
+                files: [],
                 groupedWords: [
                     { name: 'i', amount: 1234 },
                     { name: 'you', amount: 900 },
@@ -22,9 +35,18 @@
                 ]
             }
         },
+        methods: {
+            processSubtitles() {
+                console.log(this.files)
+            }
+        }
     }
 </script>
 
 <style>
-
+    .pills {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
 </style>
